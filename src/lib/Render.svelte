d542e4a0d5dd8834f0e148c8c6586eef6601a95a
jill64 <intents.turrets0h@icloud.com>
2023-12-03 00:55:17 +0900

    def lib

---
+++ src/lib/Render.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import DOMPurify from 'universal-dompurify'
+
+  export let html: string
+  export let config: DOMPurify.Config | undefined = undefined
+
+  $: sanitized = DOMPurify.sanitize(html, {
+    ...config,
+    RETURN_DOM: false,
+    RETURN_DOM_FRAGMENT: false
+  })
+</script>
+
+<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+{@html sanitized}
